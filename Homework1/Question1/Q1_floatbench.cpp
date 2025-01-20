470874df61e5b6f70e850f8e1f5d2ada5fc455f8
Justin Bahr <bahr.j@northeastern.edu>
2025-01-20 13:30:31 -0500

    Updated Q1 cpps

--- Homework1/Question1/Q1_floatbench.cpp
+++ Homework1/Question1/Q1_floatbench.cpp
@@ -1,3 +1,56 @@
 // Q1_floatbench
-// Created by justb on 1/20/2025.
-//
+// Created by Justin Bahr on 1/20/2025.
+// EECE 5640 - High Performance Computing
+// Benchmark for 100,000 floating point multiplications
+
+#include <iostream>
+#include <chrono>
+#include <ctime>
+
+using namespace std;
+
+int main()
+{
+    // defines the vector size
+    const int SIZE = 100000;
+
+    // creates input vectors a & b, and output vector c
+    float a[SIZE] = {0};
+    float b[SIZE] = {0};
+    float c[SIZE] = {0};
+
+    // seeds the random geenrator
+    srand(time(0));
+
+    // auto start_time;
+    // auto end_time;
+    // auto run_time;
+
+    // fills vectors a & b with random floats
+    for (int i = 0; i < SIZE; i++)
+    {
+        a[i] = (float)(rand()) / (float)(rand());
+        b[i] = (float)(rand()) / (float)(rand());
+    }
+
+    // initialize the high resolution clock
+    typedef chrono::high_resolution_clock clock;;
+
+    // starts the clock
+    auto start_time = clock::now();
+
+    for (int i = 0; i < SIZE; i++)
+    {
+        c[i] = a[i] * b[i];
+    }
+
+    // stops the clock
+    auto end_time = clock::now();
+
+    auto run_time = chrono::duration_cast<chrono::nanoseconds>(end_time - start_time).count();
+
+    // outputs the execution time
+    cout << "Execution time for " << SIZE << " float multiplications: " << run_time << " nanoseconds." << endl;
+
+    return 0;
+}
--- Homework1/Question1/Q1_matrixbench.cpp
+++ Homework1/Question1/Q1_matrixbench.cpp
@@ -1,3 +1,15 @@
-//
+// Q1_matrixbench
 // Created by justb on 1/20/2025.
-//
+// Benchmark for ...
+
+#include <iostream>
+#include <chrono>
+#include <ctime>
+
+using namespace std;
+
+int main()
+{
+
+    return 0;
+}
