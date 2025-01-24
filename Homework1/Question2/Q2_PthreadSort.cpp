13bb52c3fb9c2ea5a02ac21423dd2f095c7bfd04
Justin Bahr <bahr.j@northeastern.edu>
2025-01-24 10:19:02 -0500

    Added comments to Q2_PthreadSort.cpp

--- Homework1/Question2/Q2_PthreadSort.cpp
+++ Homework1/Question2/Q2_PthreadSort.cpp
@@ -15,6 +15,7 @@ void checkSorted(int* arr, int size)
 {
     bool isSorted = true;
 
+    // loops through checking if any element is less than it predecessor
     for (int i = 1; i < size; i++)
     {
         if (arr[i] < arr[i - 1])
@@ -24,6 +25,8 @@ void checkSorted(int* arr, int size)
         }
     }
 
+    // prints whether the array is sorted correctly
+    cout << "Status: ";
     if (isSorted) { cout << "Sorted Correctly" << endl; }
     else { cout << "Sorted Incorrectly" << endl; }
 }
@@ -39,32 +42,41 @@ struct ThreadArgs
 // merge function for mergeSort
 void merge(int* arr, int left, int mid, int right)
 {
+    // computes and defines the lengths of the sub arrays being merged
     int n1 = mid - left + 1;
     int n2 = right - mid;
 
+    // dynamically allocates arrays for the left and right sub arrays being merged
     int* leftArr = new int[n1];
     int* rightArr = new int[n2];
 
+    // fills the left array with the left sub array within arr
     for (int i = 0; i < n1; ++i)
     {
         leftArr[i] = arr[left + i];
     }
 
+    // fills the right array with the right sub array within arr
     for (int i = 0; i < n2; ++i)
     {
         rightArr[i] = arr[mid + 1 + i];
     }
 
+    // defines iterative variables i, j, and k to move through the left, right, and main arrays respectively
     int i = 0, j = 0, k = left;
+
+    // checks which sub array has the lower value and merges it to the next slot in the main array
     while (i < n1 && j < n2)
     {
         if (leftArr[i] <= rightArr[j]) { arr[k++] = leftArr[i++]; }
         else { arr[k++] = rightArr[j++]; }
     }
 
+    // fills the remaining values in this slice of the main array
     while (i < n1) { arr[k++] = leftArr[i++]; }
     while (j < n2) { arr[k++] = rightArr[j++]; }
 
+    // deallocates memory from the left and right sub arrays
     delete[] leftArr;
     delete[] rightArr;
 }
@@ -73,11 +85,13 @@ void merge(int* arr, int left, int mid, int right)
 // which requires that inputs have a specific signature
 void* mergeSort(void* args)
 {
+    // creates and fills ThreadArgs structures to recursively pass to mergeSort
     ThreadArgs* threadArgs = (ThreadArgs*)args;
     int* array = threadArgs->arr;
     int left = threadArgs->left;
     int right = threadArgs->right;
 
+    // recursively calls mergeSort on smaller arrays until it gets to individual elements
     if (left < right)
     {
         int mid = left + (right - left) / 2;
@@ -86,6 +100,7 @@ void* mergeSort(void* args)
         merge(array, left, mid, right);
     }
 
+    // returns a null pointer to comply with Pthreads API for start_routine
     return nullptr;
 }
 
@@ -101,6 +116,7 @@ void parallelMergeSort(int* arr, int size, int numThreads)
     // creates a pthread array of size numThreads
     pthread_t threads[numThreads];
 
+    // creates an array to store each Pthread address
     ThreadArgs args[numThreads];
 
     // defines the size for slicing the array to run in parallel
@@ -121,12 +137,13 @@ void parallelMergeSort(int* arr, int size, int numThreads)
     args[numThreads - 1] = {arr, left, right};
     pthread_create(&threads[numThreads - 1], nullptr, mergeSort, &args[numThreads - 1]);
 
-    // joins the threads once they have completed
+    // joins the Pthreads once they have completed their independent mergeSorts
     for (int i = 0; i < numThreads; i++)
     {
         pthread_join(threads[i], nullptr);
     }
 
+    // defines and initial value for this iterative variable
     int step = sliceSize;
 
     // merges each sorted slice
@@ -151,6 +168,7 @@ int main()
     // set that stores the valid numbers of threads
     set<int> threadSet = {1, 2, 4, 8, 32};
 
+    // allows the user to input a valid number of threads
     while (threadSet.find(numThreads) == threadSet.end())
     {
         cout << "How many threads would you like to use (1, 2, 4, 8, or 32)?" << endl;
