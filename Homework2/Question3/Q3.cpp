ed1812c9d610fdf38851f918fc059189250d4490
Justin Bahr <bahr.j@northeastern.edu>
2025-02-11 12:23:15 -0500

    ...

--- Homework2/Question3/Q3.cpp
+++ Homework2/Question3/Q3.cpp
@@ -116,12 +116,14 @@ int color(Graph g, int colors[], int numThreads)
             if (colors[i] > localMax)
             {
                 localMax = colors[i];
+                cout << localMax << endl;
             }
         }
 
         // critical call to update numColors with the highest local max
         #pragma omp critical
         {
+            cout << "crit: " << localMax << endl;
             if (localMax > numColors)
             {
                 numColors = localMax;
