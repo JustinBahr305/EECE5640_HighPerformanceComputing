82e9c30f9ce46a35e34f806db8c1a50da424bcc5
Justin Bahr <bahr.j@northeastern.edu>
2025-02-23 16:40:43 -0500

    Q1 and Q2 for HW3

--- Homework3/Question1/Q1.cpp
+++ Homework3/Question1/Q1.cpp
@@ -3,8 +3,84 @@
 // EECE 5640 - High Performance Computing
 // sin(x) Taylor Series using Floats and Doubles
 
+#include<iostream>
+#include <cmath>
+#include <math.h>
+#include <iomanip>
+
+using namespace std;
+
+const int numTerms = 15;
+
+int fact(int n)
+{
+    // initialize fact variable with 1
+    int fact = 1;
+
+    // loop calculating factorial
+    for (int i = 1; i <= n; i++)
+        fact *= i;
+
+    return fact;
+}
+
+float sinTaylorFloat(float x)
+{
+    float sinx = 0.0;
+    for (int n = 0; n < numTerms; n++)
+        sinx += pow(x,2*n+1)*(n % 2 == 0 ? 1.0 : -1.0)/fact(2*n+1);
+    return sinx;
+}
+
+double sinTaylorDouble(double x)
+{
+    double sinx = 0.0;
+    for (int n = 0; n < numTerms; n++)
+        sinx += pow(x,2*n+1)*(n % 2 == 0 ? 1.0 : -1.0)/fact(2*n+1);
+    return sinx;
+}
+
 int main()
 {
+    // calculates sin values
+    double sin_05 = sin(0.05);
+    double sin_5 = sin(0.5);
+    double sin1 = sin(1);
+    double sin1_5 = sin(1.5);
+
+    // calculates float taylor estimations
+    float sin_05f = sinTaylorFloat(0.05);
+    float sin_5f = sinTaylorFloat(0.5);
+    float sin1f = sinTaylorFloat(1.0);
+    float sin1_5f = sinTaylorFloat(1.5);
+
+    // calculates double taylor estimations
+    double sin_05d = sinTaylorDouble(0.05);
+    double sin_5d = sinTaylorDouble(0.5);
+    double sin1d = sinTaylorDouble(1.0);
+    double sin1_5d = sinTaylorDouble(1.5);
+
+    cout << fixed << setprecision(27);
+    cout << "Single Precision Floating Point Calculations:" << endl;
+    cout << "sin(0.05): " << sin_05f << endl;
+    cout << "sin(0.5): " << sin_5f << endl;
+    cout << "sin(1): " << sin1f << endl;
+    cout << "sin(1.5): " << sin1_5f << endl << endl;
+
+    cout << fixed << setprecision(57);
+    cout << "Double Precision Floating Point Calculations:" << endl;
+    cout << "sin(0.05): " << sin_05d << endl;
+    cout << "sin(0.5): " << sin_5d << endl;
+    cout << "sin(1): " << sin1d << endl;
+    cout << "sin(1.5): " << sin1_5d << endl << endl;
+
+    cout << "|Single Precision Error / Double Precision Error|:" << endl;
+    cout << fixed << setprecision(0);
+    cout << "0.05: " << abs((sin_05f - sin_05) / (sin_05d - sin_05)) << endl;
+    cout << "0.5: " << abs((sin_5f - sin_5) / (sin_5d - sin_5)) << endl;
+    cout << fixed << setprecision(5);
+    cout << "1: " << abs((sin1f - sin1) / (sin1d - sin1)) << endl;
+    cout << "1.5: " << abs((sin1_5f - sin1_5) / (sin1_5d - sin1_5)) << endl;
 
     return 0;
 }
--- Homework3/Question2/Q2.cpp
+++ Homework3/Question2/Q2.cpp
@@ -2,3 +2,105 @@
 // Created by Justin Bahr on 2/22/2025.
 // EECE 5640 - High Performance Computing
 // Matrix Vector Operations with AVX
+
+#include <iostream>
+#include <chrono>
+#include <x86intrin.h>
+
+using namespace std;
+
+// defines the dimension N
+const int N = 512;
+
+void matrix_vector(const float (*A)[N], const float *x, float *y, int N)
+{
+    for (int i = 0; i < N; i++)
+        for (int j = 0; j < N; j++)
+            y[i] += A[i][j] * x[j];
+}
+
+void matrix_vector_avx512f(const float (*A)[N], const float *x, float *y, int N)
+{
+    for (int i = 0; i < N; i++)
+    {
+        size_t j = 0;
+
+        // creates an array to store the y values
+        float y_arr[16];
+
+        __m512 y_vec, A_vec, x_vec;
+        y_vec = _mm512_setzero_ps();
+
+        for (; j + 16 <= N; j += 16)
+        {
+            A_vec = _mm512_loadu_ps(&A[i][j]);
+            x_vec = _mm512_loadu_ps(&x[j]);
+            y_vec = _mm512_fmadd_ps(A_vec, x_vec, y_vec);
+        }
+
+        _mm512_storeu_ps(y_arr, y_vec);
+
+        float y_sum = 0.0;
+
+        for (int k = 0; k < 16; k++)
+            y_sum += y_arr[k];
+
+        for (; j < N; j++)
+            y_sum += A[i][j] * x[j];
+
+        y[i] = y_sum;
+    }
+}
+
+int main()
+{
+    // creates an NxN matrix A and N length vectors x and y
+    float A[N][N];
+    float x[N];
+    float y1[N] = {0};
+    float y2[N] = {0};
+
+    // fills matrix A
+    for (int i = 0; i < N; i++)
+        for (int j = 0; j < N; j++)
+            A[i][j] = 0.1*(i+j);
+
+    // fills vector x
+    for (int i = 0; i < N; i++)
+        x[i] = 0.1*i;
+
+    // initialize the high resolution clock
+    typedef chrono::high_resolution_clock clock;;
+
+    // starts the clock
+    auto start_time = clock::now();
+
+    matrix_vector(A,x,y1,N);
+
+    // stops the clock
+    auto end_time = clock::now();
+
+    // casts run_time in nanoseconds
+    auto run_time1 = chrono::duration_cast<chrono::nanoseconds>(end_time - start_time).count();
+
+    // starts the clock
+    start_time = clock::now();
+
+    matrix_vector_avx512f(A,x,y2,N);
+
+    // stops the clock
+    end_time = clock::now();
+
+    // casts run_time in nanoseconds
+    auto run_time2 = chrono::duration_cast<chrono::nanoseconds>(end_time - start_time).count();
+
+    cout << "Without AVX512:" << endl;
+    cout << "y[" << N-1 << "] = " << y1[N-1] << endl;
+    cout << "Runtime in nanoseconds: " << run_time1 << endl;
+
+    cout << "With AVX512:" << endl;
+    cout << "y[" << N-1 << "] = " << y2[N-1] << endl;
+    cout << "Runtime in nanoseconds: " << run_time2 << endl;
+
+    return 0;
+}
